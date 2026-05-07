a28d680a5edb52076ff698eaff2496d12df6bd0c
parchedAlbumen <kennethpamintuan2305@gmail.com>
2026-05-06 18:12:22 -0700

    added lower and upper body set up functions @ workout.ts

--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -1,3 +1,9 @@
 export interface Exercise {
-
+    workoutType: string;
+    name: string;
+    code: string;
+    sets: number; 
+    reps: number;
+    minWeight: number;
+    maxWeight: number;
 }
--- src/helpers/workout.ts
+++ src/helpers/workout.ts
@@ -7,9 +7,68 @@ export class Workout {
     constructor() {
         this.exercises = []; //empty array for now
     }
+    
+    private createBasicWorkout(): void {
+        this.addUpperBody();
+        this.addLowerBody();
+    }
+
+    private addUpperBody(): void {
+        this.createExercise("upper", "Barbell Bench Press", "bbp", 4, 5, 0, 0);
+        this.createExercise("upper", "Barbell Row", "br", 4, 6, 0, 0);
+        this.createExercise("upper", "Chest Supported Row", "csr", 4, 6, 0, 0);
+        this.createExercise("upper", "Weighted Pull-Ups", "wpu", 3, 6, 0, 0);
+        this.createExercise("upper", "Lat Pulldown", "lpd", 4, 8, 0, 0);
+        this.createExercise("upper", "Seated DB Shoulder Press", "sdbsp", 3, 6, 0, 0);
+        this.createExercise("upper", "Incline DB Press", "idbp", 4, 8, 0, 0);
+        this.createExercise("upper", "Face Pulls", "fp", 3, 12, 0, 0);
+        this.createExercise("upper", "Lateral Raises", "lr", 3, 12, 0, 0);
+        this.createExercise("upper", "Overhead Triceps Extension", "ote", 3, 8, 0, 0);
+        this.createExercise("upper", "Barbell Curls", "bbc", 3, 6, 0, 0);
+        this.createExercise("upper", "EZ-Bar Curls", "ezc", 3, 6, 0, 0);
+        this.createExercise("upper", "Cable Fly", "cf", 3, 10, 0, 0);
+        this.createExercise("upper", "Pec Deck", "pd", 3, 10, 0, 0);
+        this.createExercise("upper", "Seated Cable Row", "scr", 3, 10, 0, 0);
+        this.createExercise("upper", "Cable Pullover", "cp", 3, 10, 0, 0);
+        this.createExercise("upper", "DB Pullover", "dbp", 3, 10, 0, 0);
+        this.createExercise("upper", "Rear Delt Fly", "rdf", 3, 15, 0, 0);
+        this.createExercise("upper", "Rope Pushdowns", "rpd", 3, 12, 0, 0);
+        this.createExercise("upper", "Hammer Curls", "hc", 3, 10, 0, 0);
+        this.createExercise("upper", "Incline DB Curls", "idbc", 3, 10, 0, 0);
+    }
+
+    private addLowerBody() {
+        this.createExercise("lower", "Squat", "sq", 4, 8, 0, 0);
+        this.createExercise("lower", "Romanian Deadlift", "rdl", 4, 8, 0, 0);
+        this.createExercise("lower", "Stationary Lunges", "sl", 3, 8, 0, 0);
+        this.createExercise("lower", "Leg Extension", "le", 3, 10, 0, 0);
+        this.createExercise("lower", "Leg Curl", "lc", 3, 10, 0, 0);
+        this.createExercise("lower", "Hip Adductor Machine", "ham", 3, 12, 0, 0);
+        this.createExercise("lower", "Calf Raises", "cr", 4, 12, 0, 0);
+    }
+
+    private createExercise(workoutType: string, name: string, code: string, sets: number, reps: number, minWeight: number, maxWeight: number): Exercise {
+        return {
+            workoutType,
+            name,
+            code,
+            sets,
+            reps,
+            maxWeight,
+            minWeight
+        }
+    }
 
     public createBasicTemplate(): string {
-        return "hello";
+        let template = "";
+        this.exercises.forEach((exer) => {
+            template += (this.exerciseSummary(exer) + "\n");
+        })
+        return template;
+    }
+
+    private exerciseSummary(exer: Exercise): string {
+        return `[${exer.workoutType.toUpperCase()}] ${exer.code.toUpperCase()} - ${exer.name} (${exer.sets}x${exer.reps}) | ${exer.minWeight}-${exer.maxWeight}kg`;
     }
 }
 
